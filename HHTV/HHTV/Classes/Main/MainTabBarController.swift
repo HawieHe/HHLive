3da96ba4eb046ffd8ff54fe95b412115bf012cd6
Hawie <hehui1001@qq.com>
2016-12-29 14:52:40 +0800

    搭建项目主框架

---
+++ HHTV/HHTV/Classes/Main/MainTabBarController.swift
@@ -0,0 +1,20 @@
+//
+//  MainTabBarController.swift
+//  HHTV
+//
+//  Created by HeHui on 2016/12/29.
+//  Copyright © 2016年 Hawie. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Do any additional setup after loading the view.
+    }
+
+  }
